f26c5735f213611dcdba656241b22023c7e0fcd4
Fran <fransanxenxo@gmail.com>
2022-05-28 18:52:52 +0100

    feat: countdown clock

--- react-countdown/src/App.js
+++ react-countdown/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+
 
 function App() {
+
+  const [timerDays, setTimerDays] = useState('00')
+  const [timerHours, setTimerHours] = useState('00')
+  const [timerMinutes, setTimerMinutes] = useState('00')
+  const [timerSeconds, setTimerSeconds] = useState('00')
+
+
+  let interval = useRef()
+
+  const startTimer =() => {
+    const countdownDate = new Date('June 23, 2022  17:00:00').getTime()
+
+    interval = setInterval(() => {
+
+      
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+       clearInterval(interval.current)
+      }else{
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+
+    }, 1000)
+  }
+
+  useEffect(() => {
+    startTimer()
+    return () => {
+      clearInterval(interval.current)
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <section>
+       <p>{timerDays}</p>
+       <p>{timerHours}</p>
+       <p>{timerMinutes}</p>
+       <p>{timerSeconds}</p>
+     </section>
     </div>
   );
 }
